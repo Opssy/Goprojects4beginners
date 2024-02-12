cea03fd54bd46bc808255c27b3ad64448dc510f7
Opeyemi <opeyemib39@gmail.com>
2024-02-12 17:44:27 +0100

    simple go calculator completed

--- calculator/main.go
+++ calculator/main.go
@@ -4,4 +4,26 @@ import "fmt"
 
 func calculator(){
 	 
+	var operator string;
+	fmt.Println("Enter operator: +, -, *, /")
+	fmt.Scan(&operator)
+
+
+	var a,b float64
+
+	fmt.Println("Enter your two digits: ")
+	fmt.Scan(&a, &b)
+
+	switch choice{
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+    case "/":
+		fmt.Println(a / b)
+	 default:
+        fmt.Println("Invalid operator")
+	}
 }
