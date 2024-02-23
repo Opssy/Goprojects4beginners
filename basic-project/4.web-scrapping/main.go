dbdb35b3760eef7f6ecee9411181db62cc8356ad
Opeyemi <opeyemib39@gmail.com>
2024-02-23 22:06:21 +0100

    basic setup

--- basic-project/4.web-scrapping/main.go
+++ basic-project/4.web-scrapping/main.go
@@ -1,6 +1,25 @@
 package main
 
+import (
+ "fmt"
+ "log"
+ "strings"
 
+ "github.com/gocolly/colly/v2"
+)
 func main() {
+   // create a new colly collector
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+	)
 
+   c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+
+	 link := e.Attr("href")
+
+	 fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+      
+	 
+   })
 }
