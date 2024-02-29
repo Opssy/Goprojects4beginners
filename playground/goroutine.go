b324b2132724fcdb4ca00b890549e008602bf521
Opeyemi <opeyemib39@gmail.com>
2024-02-29 09:20:37 +0100

    goroutine buffered channel

--- playground/goroutine.go
+++ playground/goroutine.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
-
-// goroutine example 
+// goroutine example
 
 // 1. main goroutine starts
 // 2. Invokes helloworld and helloworld goroutine starts
@@ -73,16 +73,62 @@ func simpleChannel(){
 //buffered channel 
 
 func BufferedChannel(){
-	// Create a buffered channel of type int with capacity of 5
-	c := make(chan int, 5)
+	// // Create a buffered channel of type int with capacity of 5
+	// c := make(chan int, 5)
 
-	// Send 5 values to the channel
-	for i := 0; i < 5; i++ {
-		c <- i
-	}
+	// // Send 5 values to the channel
+	// for i := 0; i < 5; i++ {
+	// 	c <- i
+	// }
 
-	// Receive and print the values from the channel
-	for i := 0; i < 5; i++ {
-		fmt.Println(<-c)
-	}
+	// // Receive and print the values from the channel
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	//create a buffered channel of type string with capacity of 5
+
+	cars := make(chan string, 5)
+
+	// Start five Goroutines that add cars to the channel
+	go addCar("BMW", cars)
+	go addCar("Mercedes", cars)
+	go addCar("Ferrari", cars)
+	go addCar("Toyota", cars)
+	go addCar("Honda", cars)
+
+	// Wait for the cars to be added to the channel
+	time.Sleep(2 * time.Second)
+	close(cars)
+
+	//Start a Goroutine that simulates the race
+	go startRace(cars)
+
+	// Wait for the race to finish
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("Race over!")
+}
+
+// to add a car to the channel 
+func addCar(name string, cars chan string) {
+	cars <- name
+	fmt.Println(name, "added to the race!")
+} 
+
+func startRace(cars chan string) {
+	for {
+	  // Receive a car from the channel
+    car, open := <-cars
+    if !open {
+        break
+    }
+
+    fmt.Println(car, "is racing...")
+	
+     // Simulate the race by waiting for a random duration
+    time.Sleep(time.Duration(1+rand.Intn(5)) * time.Second)
+    }
+   fmt.Println("All cars have finished the race!")
+
 }
